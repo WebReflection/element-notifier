9a1e995adf4801e1ed7a54410612befb12d66436
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-04-13 11:14:28 +0200

    first commit

---
+++ esm/index.js
@@ -0,0 +1,40 @@
+const TRUE = true, FALSE = false;
+const QSA = 'querySelectorAll';
+
+export const notify = callback => {
+  const loop = (nodes, added, removed, connected, pass) => {
+    for (let i = 0, {length} = nodes; i < length; i++) {
+      const node = nodes[i];
+      if (pass || (QSA in node)) {
+        if (connected) {
+          if (!added.has(node)) {
+            added.add(node);
+            removed.delete(node);
+            callback(node, connected);
+          }
+        }
+        else if (!removed.has(node)) {
+          removed.add(node);
+          added.delete(node);
+          callback(node, connected);
+        }
+        if (!pass)
+          loop(node[QSA]('*'), added, removed, connected, TRUE);
+      }
+    }
+  };
+  const observer = new MutationObserver(records => {
+    for (let
+      added = new Set,
+      removed = new Set,
+      i = 0, {length} = records;
+      i < length; i++
+    ) {
+      const {addedNodes, removedNodes} = records[i];
+      loop(removedNodes, added, removed, FALSE, FALSE);
+      loop(addedNodes, added, removed, TRUE, FALSE);
+    }
+  });
+  observer.observe(document, {subtree: TRUE, childList: TRUE});
+  return observer;
+};
---
+++ index.js
@@ -0,0 +1,50 @@
+self.elementNotifier = (function (exports) {
+  'use strict';
+
+  var TRUE = true,
+      FALSE = false;
+  var QSA = 'querySelectorAll';
+  var notify = function notify(callback) {
+    var loop = function loop(nodes, added, removed, connected, pass) {
+      for (var i = 0, length = nodes.length; i < length; i++) {
+        var node = nodes[i];
+
+        if (pass || QSA in node) {
+          if (connected) {
+            if (!added.has(node)) {
+              added.add(node);
+              removed["delete"](node);
+              callback(node, connected);
+            }
+          } else if (!removed.has(node)) {
+            removed.add(node);
+            added["delete"](node);
+            callback(node, connected);
+          }
+
+          if (!pass) loop(node[QSA]('*'), added, removed, connected, TRUE);
+        }
+      }
+    };
+
+    var observer = new MutationObserver(function (records) {
+      for (var added = new Set(), removed = new Set(), i = 0, length = records.length; i < length; i++) {
+        var _records$i = records[i],
+            addedNodes = _records$i.addedNodes,
+            removedNodes = _records$i.removedNodes;
+        loop(removedNodes, added, removed, FALSE, FALSE);
+        loop(addedNodes, added, removed, TRUE, FALSE);
+      }
+    });
+    observer.observe(document, {
+      subtree: TRUE,
+      childList: TRUE
+    });
+    return observer;
+  };
+
+  exports.notify = notify;
+
+  return exports;
+
+}({}));
